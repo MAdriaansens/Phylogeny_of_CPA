66c0fd60458020dab9e70027e2031260797e1023
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-05-09 15:58:03 +1200

    Update Create_TSV_proteinDB_from_GTDB_data.py

--- Database_creation/Create_TSV_proteinDB_from_GTDB_data.py
+++ Database_creation/Create_TSV_proteinDB_from_GTDB_data.py
@@ -1,16 +1,16 @@
 import os
 from Bio import SeqIO
-
+import sys
 
 #this requires that you got the metadata as well as the whole GTDB proteome
 #the tsv can then be run to generate the db in a fasta format
 
-tsv_file = '/nesi/nobackup/uc04105/database/GTDB_220/ar53_metadata_r220.tsv'
-dir_files = '/nesi/nobackup/uc04105/database/GTDB_220/protein_faa_reps/archaea'
-files = os.listdir('/nesi/nobackup/uc04105/database/GTDB_220/protein_faa_reps/archaea')
+tsv_file = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/bac120_metadata.tsv'
+dir_files = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/protein_faa_reps/bacteria'
+files = os.listdir('/nesi/nobackup/uc04105/new_databases_May/GTDB_226/protein_faa_reps/bacteria')
 
-outfile = '/nesi/nobackup/uc04105/database/GTDB_220/Archaea_GTDBprotein_0608024.tsv'
-
+outfile = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bacteria_GTDB226_protein_May92025.tsv'
+fasta_out = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bacteria_GTDB226_protein_May92025.faa'
 dictionary = {}
 
 with open('{}'.format(tsv_file)) as dataset:
@@ -22,23 +22,22 @@ with open('{}'.format(tsv_file)) as dataset:
 
 header = 'GTDB_id' + '\t' + 'GTDB_taxonomy' + '\t' + 'Protein_id_GTDB' + '\t' + 'Sequence' + '\t' + 'Protein_id_annotation' + '\n'
 
-
 with open(outfile, 'a') as f:
-    f.write(header)
-f.close()
+   f.write(header)
 
-
-with open(outfile, 'a') as f:
-    count = 0
-    for file in files:
-
-        GTDB_ID = file.split('_protein.faa')[0]
-        taxonomy = dictionary[GTDB_ID]
-        for record in SeqIO.parse('{}/{}'.format(dir_files, file), "fasta"):
-            count = count + 1
-            Protein_id_GTDB = record.id
-            Sequence = str(record.seq)
-            Protein_id_annotation = 'Arc_{}'.format(count)
-            line = Protein_id_annotation + '\t' + Protein_id_GTDB + '\t' + taxonomy + '\t' + GTDB_ID + '\t' + Sequence + '\n'
-            f.write(line)
+    with open(fasta_out, 'a') as fo:
+        count = 0
+        for file in files:
+    
+            GTDB_ID = file.split('_protein.faa')[0]
+            taxonomy = dictionary[GTDB_ID]
+            for record in SeqIO.parse('{}/{}'.format(dir_files, file), "fasta"):
+                count = count + 1
+                Protein_id_GTDB = record.id
+                Sequence = str(record.seq)
+                Protein_id_annotation = 'Bac226_{}'.format(count)
+                line = Protein_id_annotation + '\t' + Protein_id_GTDB + '\t' + taxonomy + '\t' + GTDB_ID + '\t' + Sequence + '\n'
+                fasta_line = '>' + Protein_id_annotation + '\n' + Sequence + '\n'
+                #fo.write(fasta_line)
+                #f.write(line)
 f.close()
