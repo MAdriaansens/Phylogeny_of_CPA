e7aa94ee16d7876c35bfe1684637eb6f27e4975a
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-06-13 10:46:37 +1200

    Create subset_Bac_data.py

---
+++ Database_creation/subset_Bac_data.py
@@ -0,0 +1,33 @@
+tinput = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bacteria_GTDB226_protein_May92025.faa'
+
+
+def batch_iterator(iterator, batch_size):
+    """Returns lists of length batch_size.
+
+    This can be used on any iterator, for example to batch up
+    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+    Alignment objects from Bio.Align.parse(...), or simply
+    lines from a file handle.
+
+    This is a generator function, and it returns lists of the
+    entries from the supplied iterator.  Each list will have
+    batch_size entries, although the final list may be shorter.
+    """
+    batch = []
+    for entry in iterator:
+        batch.append(entry)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+from Bio import SeqIO
+
+record_iter = SeqIO.parse(open(tinput), "fasta")
+for i, batch in enumerate(batch_iterator(record_iter, 7070931)):
+    filename = "/nesi/nobackup/uc04105/new_databases_May/GTDB_226/DB/Bacteria_GTDB226_protein_May92025_subset%i.fasta" % (i + 1)
+    with open(filename, "w") as handle:
+        count = SeqIO.write(batch, handle, "fasta")
+    print("Wrote %i records to %s" % (count, filename))
+~                                                          
