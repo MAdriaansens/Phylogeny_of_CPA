5588f31a9796d5b41bb3216ea512b2fbf495759c
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-06-15 11:20:27 +1200

    Create pre_filter_pfam.py

---
+++ Database_creation/pre_filter_pfam.py
@@ -0,0 +1,13 @@
+from Bio import SeqIO
+import sys
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+with open('/nesi/nobackup/uc04105/fasta_files/pfams/{}'.format(infile), 'a') as S:
+    for record in SeqIO.parse('/nesi/nobackup/uc04105/fasta_files/pfams/{}'.format(outfile)', 'fasta'):
+        if len(record.seq) < 257: #257 in case of PF00999
+            pass
+        else:
+            outline = '>' + record.id + '\n' + str(record.seq)
+            S.write(outline)
