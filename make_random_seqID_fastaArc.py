dcf3420099fedfdf805a14faa078708eba645b95
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-06-16 14:53:11 +1200

    Update make_random_seqID_fastaArc.py
    missess a check to see if it is really 100

--- make_random_seqID_fastaArc.py
+++ make_random_seqID_fastaArc.py
@@ -1,39 +1,40 @@
 import json
 from Bio import SeqIO
 import random
-direct ='/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMscan/Archaea'
-HMS00999='Archaea_PF00999_aligned_hmmscanned_onlyPF00999.json'
-outdir = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Archaea/intermediate_PF00999/random_list'
+#if from fasta file directly:
+CPA_id_list= []
+HMMaligned = '/nesi/nobackup/uc04105/fasta_files/Seed_NhaA/SG_seed_update_jan142024_135seq_PF00999aligned.fasta.fasta'
+outdir ='/nesi/nobackup/uc04105/fasta_files/Seed_NhaA'
+for record in SeqIO.parse(HMMaligned, 'fasta'):
+   CPA_id_list.append(record.id.split('|')[1])
+CPA_id_list = list(set(CPA_id_list))
 
-
-CPA_id_list=[]
-with open('{}/{}'.format(direct, HMS00999)) as json_file:
-    CPA_data=json.load(json_file)
-    for key in CPA_data.keys():
-        output = CPA_data[key]
-        if output[0] == 'PF00999':
-            CPA_id_list.append(key.split('_tax:')[0])
-
-HMMaligned = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Archaea/intermediate_PF00999/PF00999_Archaea_MERGED_alignedPF00999.faa'
-
-def return_fasta_from_list(HMMaligned, random_list, count):
+def return_fasta_from_list(HMMaligned, random_list2, count, outdir):
     written_list = []
-    with open('{}/Arc_HMMscanned_PF00999_aligned_randomset_{}.fasta'.format(outdir, count), 'a') as OUT:
+    with open('{}/Seed_HMMscanned_PF00999_aligned_randomset_{}.fasta'.format(outdir, count), 'a') as OUT:
         for record in SeqIO.parse(HMMaligned, 'fasta'):
-
-            if record.id.split('_tax:')[0].split('|')[1] not in random_list:
+            if record.id.split('|')[1] not in random_list2:
                 pass
             else:
-                if record.id.split('_tax:')[0].split('|')[1] not in written_list:
+                if record.id.split('|')[1] not in written_list:
                     out_fasta = '>' + record.id.split('|')[1] + '\n' + str(record.seq) + '\n'
-
-                    written_list.append(record.id.split('_tax:')[0].split('|')[1])
+                    written_list.append(record.id.split('|')[1])
                     OUT.write(out_fasta)
                 else:
                     pass
+
 for count in range(1,101):
-    random_list = []
     random_list = random.choices(CPA_id_list, k=100)
-    print(len(random_list))
+    if len(list(set(random_list))) != 100:
+        for i in random.sample(CPA_id_list, k=len(CPA_id_list)):
+            if len(list(set(random_list))) < 100:
+                random_list.append(i)
+            elif len(list(set(random_list))) == 100:
+                break
+    random_list2 = list(set(random_list))
+    print(len(random_list2))
+    random_list = []
+    return_fasta_from_list(HMMaligned, random_list2, count, outdir)
+    
     return_fasta_from_list(HMMaligned, random_list, count)
     
