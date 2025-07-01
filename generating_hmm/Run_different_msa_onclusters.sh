4418f279e68676bcd8f75636ed16d7908314871a
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-07-02 11:53:59 +1200

    Update Run_different_msa_onclusters.sh

--- generating_hmm/Run_different_msa_onclusters.sh
+++ generating_hmm/Run_different_msa_onclusters.sh
@@ -20,24 +20,24 @@ declare -a array=("0.6" "0.7" "0.8" "0.9")
 
 mafft --thread 25 --auto ${MMseq}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq.fasta > ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_autoaligned.faa
 
-module load HMMER/3.4-GCC-12.3.0
+module load HMMER/3.3.2-GCC-12.3.0
 
 hmmbuild ${HMM_out}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_autoaligned.hmm ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_autoaligned.faa
 
 mafft --thread 25 --globalpair ${MMseq}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq.fasta > ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_globalpairaligned.faa
 
-#module load HMMER/3.4-GCC-12.3.0
+#module load HHMMER/3.3.2-GCC-12.3.0
 hmmbuild ${HMM_out}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_globalpairaligned.hmm ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_globalpairaligned.faa
 
 mafft --thread 25 --localpair ${MMseq}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq.fasta > ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_localpairaligned.faa
 
-#module load HMMER/3.4-GCC-12.3.0
+#module load HMMER/3.3.2-GCC-12.3.0
 
 hmmbuild ${HMM_out}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_localpairaligned.hmm ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_localpairaligned.faa
 
 
 mafft --thread 25 --genafpair ${MMseq}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq.fasta > ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_genafpairaligned.faa
 
-#module load HMMER/3.4-GCC-12.3.0
+#module load HMMER/3.3.2-GCC-12.3.0
 
 hmmbuild ${HMM_out}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_genafpairaligned.hmm ${MAFFT}/Archaea_Manual_seq_e05_cov30_seqid${array[$SLURM_ARRAY_TASK_ID]}.faa_rep_seq_genafpairaligned.faa
