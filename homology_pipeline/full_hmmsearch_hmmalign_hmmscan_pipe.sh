13d68d7f9759a1b1f86c7ed7e479611b06f2acb0
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-07-06 10:30:18 +1200

    Update full_hmmsearch_hmmalign_hmmscan_pipe.sh

--- homology_pipeline/full_hmmsearch_hmmalign_hmmscan_pipe.sh
+++ homology_pipeline/full_hmmsearch_hmmalign_hmmscan_pipe.sh
@@ -19,30 +19,29 @@ module load HMMER/3.3.2-GCC-12.3.0
 module load Python/3.11.6-foss-2023a
 
 
-#hmmsearch --noali --cpu 15 -E 0.001 --tblout ${HMMsearch}/BAC_HMM_e03vsArchaea.tsv ${BACHMM} ${DB}
+hmmsearch --noali --cpu 15 -E 0.001 --tblout ${HMMsearch}/BAC_HMM_e03vsArchaea.tsv ${BACHMM} ${DB}
 
-#python getting_fasta_from_hit_extra.py ${HMMsearch}/BAC_HMM_e03vsArchaea.tsv HMM ${TSV} ${HMMsearch}/BAC_HMM_e03vsArchaea.fa
+python getting_fasta_from_hit_extra.py ${HMMsearch}/BAC_HMM_e03vsArchaea.tsv HMM ${TSV} ${HMMsearch}/BAC_HMM_e03vsArchaea.fa
 
-#hmmalign --amino --trim -o ${HMMalign}/BAC_HMM_e03vsArchaea.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm ${HMMsearch}/BAC_HMM_e03vsArchaea.fa
+hmmalign --amino --trim -o ${HMMalign}/BAC_HMM_e03vsArchaea.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm ${HMMsearch}/BAC_HMM_e03vsArchaea.fa
 
-#python parse_stockholm_filter.py ${HMMalign}/BAC_HMM_e03vsArchaea.sthk ${HMMalign}/BAC_HMM_e03vsArchaea.fa 258
-#mkdir -p ${HMMalign}/HMMsearch
+python parse_stockholm_filter.py ${HMMalign}/BAC_HMM_e03vsArchaea.sthk ${HMMalign}/BAC_HMM_e03vsArchaea.fa 258
 
-#python retrieve_fl.py BAC_HMM_e03vsArchaea.fa.fasta BAC_HMM_e03vsArchaea_fulllength.fa.fasta ${HMMalign} ${TSV}
-
-
-#hmmsearch --noali --cpu 15 -E 0.001 --tblout ${HMMsearch}/EUK_HMM_e03vsArchaea.tsv ${EUKHMM} ${DB}
-
-#python getting_fasta_from_hit_extra.py ${HMMsearch}/EUK_HMM_e03vsArchaea.tsv HMM ${TSV} ${HMMsearch}/EUK_HMM_e03vsArchaea.fa
-
-#hmmalign --amino --trim -o ${HMMalign}/EUK_HMM_e03vsArchaea.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm ${HMMsearch}/EUK_HMM_e03vsArchaea.fa
-
-#python parse_stockholm_filter.py ${HMMalign}/EUK_HMM_e03vsArchaea.sthk ${HMMalign}/EUK_HMM_e03vsArchaea.fa 258
-
-#python retrieve_fl.py EUK_HMM_e03vsArchaea.fa.fasta EUK_HMM_e03vsArchaea_fulllength.fa.fasta ${HMMalign} ${TSV}
+python retrieve_fl.py BAC_HMM_e03vsArchaea.fa.fasta BAC_HMM_e03vsArchaea_fulllength.fa.fasta ${HMMalign} ${TSV}
 
 
-#hmmscan --tblout ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_INHhmmscanned.tsv /nesi/nobackup/uc04105/results/HMM/HMM_db/INH_HMM_PF00999.hmm ${HMMalign}/BAC_HMM_e03vsArchaea_fulllength.fa.fasta
+hmmsearch --noali --cpu 15 -E 0.001 --tblout ${HMMsearch}/EUK_HMM_e03vsArchaea.tsv ${EUKHMM} ${DB}
+
+python getting_fasta_from_hit_extra.py ${HMMsearch}/EUK_HMM_e03vsArchaea.tsv HMM ${TSV} ${HMMsearch}/EUK_HMM_e03vsArchaea.fa
+
+hmmalign --amino --trim -o ${HMMalign}/EUK_HMM_e03vsArchaea.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm ${HMMsearch}/EUK_HMM_e03vsArchaea.fa
+
+python parse_stockholm_filter.py ${HMMalign}/EUK_HMM_e03vsArchaea.sthk ${HMMalign}/EUK_HMM_e03vsArchaea.fa 258
+
+python retrieve_fl.py EUK_HMM_e03vsArchaea.fa.fasta EUK_HMM_e03vsArchaea_fulllength.fa.fasta ${HMMalign} ${TSV}
+
+
+hmmscan --tblout ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_INHhmmscanned.tsv /nesi/nobackup/uc04105/results/HMM/HMM_db/INH_HMM_PF00999.hmm ${HMMalign}/BAC_HMM_e03vsArchaea_fulllength.fa.fasta
 
 python parse_hmmscan.py ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_INHhmmscanned.tsv PF00999 ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_INHhmmscanned.json
 
@@ -50,8 +49,6 @@ hmmscan --tblout ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_Pfamhmmscanned.
 
 python parse_hmmscan.py ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_Pfamhmmscanned.tsv PF00999 ${HMMscan}/BAC_HMM_e03vsArchaea_PF00999_aligned_Pfamhmmscanned.json
 
-
-#mkdir -p ${HMMalign}/HMMsearch
 
 
 hmmscan --tblout ${HMMscan}/EUK_HMM_e03vsArchaea_PF00999_aligned_INHhmmscanned.tsv /nesi/nobackup/uc04105/results/HMM/HMM_db/INH_HMM_PF00999.hmm ${HMMalign}/EUK_HMM_e03vsArchaea_fulllength.fa.fasta
