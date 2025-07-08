2745d71b1fbb78bfe466439fd3efaafa345ac417
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-07-09 11:56:28 +1200

    Update parse_hmmscan_euk_arc.py

--- Data_analysis/parse_hmmscan_euk_arc.py
+++ Data_analysis/parse_hmmscan_euk_arc.py
@@ -41,93 +41,20 @@ scan_dict = {}
 scan_dict = best_hit_dict(HMMscan)
 
 scan_dict = best_hit_dict(HMMscan)
+HMMscan = '/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMscan/Archaea/BAC_HMM_e03vsArchaea_hmmsearchFULLPFAMscanned.tsv' #PFAM
+scan_dict = {}
+
+
+scan_dict = best_hit_dict(HMMscan, scan_dict)
 
 Pfam_count = 0
 Eukcount =0
 Arc_count =0
 Bac_count = 0
 count = 0
+Pfam_hitlist = []
 for key in scan_dict.keys():
     count =  count + 1
-    if 'Na_H_Exchanger' == (scan_dict[key][0]):
-        Pfam_count = Pfam_count + 1
-    if 'Manual_seq_cov30_e05_seqid0.7_genafpair_aligned' == (scan_dict[key][0]):
-        Eukcount = Eukcount + 1
-    if 'Archaea_Manual_seq_e05_cov30_seqid0.6.faa_rep_seq_globalpairaligned' == (scan_dict[key][0]):
-        Arc_count = Arc_count + 1
-    elif 'Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned' == (scan_dict[key][0]):
-        Bac_count = Bac_count + 1
-print((Pfam_count/count)*100, (Bac_count/count)*100, (Eukcount/count)*100, (Arc_count/count)*100)
-
-HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/BAC_HMM_e03vsEukarya_fl_INH_scanned.tsv'#BAC HMM
-
-
-
-scan_dict = {}                    
-scan_dict = best_hit_dict(HMMscan)
-
-Pfam_count = 0
-Eukcount =0
-Arc_count =0
-Bac_count = 0
-count = 0
-for key in scan_dict.keys():
-    count =  count + 1
-    if 'Na_H_Exchanger' == (scan_dict[key][0]):
-        Pfam_count = Pfam_count + 1
-    if 'Manual_seq_cov30_e05_seqid0.7_genafpair_aligned' == (scan_dict[key][0]):
-        Eukcount = Eukcount + 1
-    if 'Archaea_Manual_seq_e05_cov30_seqid0.6.faa_rep_seq_globalpairaligned' == (scan_dict[key][0]):
-        Arc_count = Arc_count + 1
-    elif 'Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned' == (scan_dict[key][0]):
-        Bac_count = Bac_count + 1
-print((Pfam_count/count)*100, (Bac_count/count)*100, (Eukcount/count)*100, (Arc_count/count)*100)
-
-#HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/Eukarya_allmerged_PF00999_INH_HMMSCANNED.TSV' #EUK TO be done
-
-
-
-scan_dict = {}                    
-scan_dict = best_hit_dict(HMMscan)
-
-Pfam_count = 0
-Eukcount =0
-Arc_count =0
-Bac_count = 0
-count = 0
-for key in scan_dict.keys():
-    count =  count + 1
-    if 'Na_H_Exchanger' == (scan_dict[key][0]):
-        Pfam_count = Pfam_count + 1
-    if 'Manual_seq_cov30_e05_seqid0.7_genafpair_aligned' == (scan_dict[key][0]):
-        Eukcount = Eukcount + 1
-    if 'Archaea_Manual_seq_e05_cov30_seqid0.6.faa_rep_seq_globalpairaligned' == (scan_dict[key][0]):
-        Arc_count = Arc_count + 1
-    elif 'Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned' == (scan_dict[key][0]):
-        Bac_count = Bac_count + 1
-print((Pfam_count/count)*100, (Bac_count/count)*100, (Eukcount/count)*100, (Arc_count/count)*100)
-
-#HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/Eukarya_allmerged_PF00999_INH_HMMSCANNED.TSV' #EUK TO be done
-HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/ARC_HMM_e03vsEukarya_fl_INH_scanned.tsv' #ARC HMM
-
-
-
-scan_dict = {}                    
-scan_dict = best_hit_dict(HMMscan)
-
-Pfam_count = 0
-Eukcount =0
-Arc_count =0
-Bac_count = 0
-count = 0
-for key in scan_dict.keys():
-    count =  count + 1
-    if 'Na_H_Exchanger' == (scan_dict[key][0]):
-        Pfam_count = Pfam_count + 1
-    if 'Manual_seq_cov30_e05_seqid0.7_genafpair_aligned' == (scan_dict[key][0]):
-        Eukcount = Eukcount + 1
-    if 'Archaea_Manual_seq_e05_cov30_seqid0.6.faa_rep_seq_globalpairaligned' == (scan_dict[key][0]):
-        Arc_count = Arc_count + 1
-    elif 'Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned' == (scan_dict[key][0]):
-        Bac_count = Bac_count + 1
-print((Pfam_count/count)*100, (Bac_count/count)*100, (Eukcount/count)*100, (Arc_count/count)*100)
+    Pfam_hitlist.append(scan_dict[key][0])
+print(count)
+print(Counter(Pfam_hitlist))
