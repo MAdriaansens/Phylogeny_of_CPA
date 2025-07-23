40d6959451fb2ea382dc27dcdf752f0b1ac8580f
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-07-23 15:10:59 +1200

    Update parse_hmmscan_hmmalign_euk.py

--- Data_analysis/parse_hmmscan_hmmalign_euk.py
+++ Data_analysis/parse_hmmscan_hmmalign_euk.py
@@ -35,16 +35,38 @@ def best_hit_dict(HMMscan, scan_dict):
                     scan_dict[full_id] =entry_list
     return(scan_dict)
 
-scanned_dict = {}
+CPA_hit_list = ['Archaea_Manual_e5_cov50_AlignedPF00999_clustered_0.6_rep_seq_Ginsialigned', 'Na_H_Exchanger', 'Manual_seq_cov30_e05_seqid0.7_genafpair_aligned', 'Na_H_antiport_1', 'CHX17_2nd', 'CHX_17', 'Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned']
+scanned_list = []
+HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/cross_domain/Self_6juli_merged_MMSEQ_vsEukarya_PF00999hmmaligned_fl_FULLPFAMHMMscanned.tsv' #PFAM
+scan_dict = {}
 
-print(len(scanned_dict.keys()))
 
-for key in scanned_dict.keys():
-    print(key)
-    break
-from Bio import SeqIO
+scan_dict = best_hit_dict(HMMscan, scan_dict)
 
-import os
+for key in scan_dict.keys():
+    if scan_dict[key][0] in CPA_list:
+        scanned_list.append(key)
+
+HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/cross_domain/7JULI_ARCHAEA_vsEukarya_PF00999hmmaligned_fl_FULLPFAMHMMscanned.tsv' #PFAM
+scan_dict = {}
+
+
+scan_dict = best_hit_dict(HMMscan, scan_dict)
+
+for key in scan_dict.keys():
+    if scan_dict[key][0] in CPA_list:
+        scanned_list.append(key)
+        
+HMMscan = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMscan/cross_domain/7JULI_BACTERIA_vsEukarya_PF00999hmmaligned_fl_FULLPFAMHMMscanned.tsv' #PFAM
+scan_dict = {}
+
+
+scan_dict = best_hit_dict(HMMscan, scan_dict)
+
+for key in scan_dict.keys():
+    if scan_dict[key][0] in CPA_list:
+        scanned_list.append(key)
+
 
 HMMalign_dir = '/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMalign/cross_domain'
 Seq_dict = {}
@@ -52,11 +74,10 @@ for j in os.listdir(HMMalign_dir):
     if 'fa.fasta' in j:
         for record in SeqIO.parse('{}/{}'.format(HMMalign_dir, j), 'fasta'):
             Seq_dict[record.id] = record.seq
-
 with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/sequences/Euk_hmmscanned_hmmaligned.fasta', 'a') as O:
 
-    for key in scanned_dict.keys():
-        sequence_id = key.split('_tax')[0]
-        sequence = Seq_dict[key]
+    for entry in set(scanned_list):
+        sequence_id = entry.split('_tax')[0]
+        sequence = Seq_dict[entry]
         Line = '>{}'.format(sequence_id) + '\n' + str(sequence) + '\n'
         O.write(Line)
