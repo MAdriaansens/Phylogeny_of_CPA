bf31e8b29af38b307c91888369f093874d3a7b89
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-07-24 13:27:33 +1200

    Update Get_taxonomy_clusteroids.py

--- Data_analysis/Get_taxonomy_clusteroids.py
+++ Data_analysis/Get_taxonomy_clusteroids.py
@@ -1,29 +1,34 @@
-directory = '/nesi/nobackup/uc04105/fasta_files/pipeline/CPA/tree_16082024/input_files_tree/cat/cluster_mmseqs'
 
-diction = '/nesi/nobackup/uc04105/Bacteria_hits_full_mar17.json'
-tsv_cluster = '{}/BacteriaclusterResSI7C8_cluster.tsv'.format(directory)
-reps = '{}/BacteriaclusterResSI7C8_rep_seq.fasta'.format(directory)
+from Bio import SeqIO
 
 #diction contains all the protein_ids and matching accession id + taxonomy
 
 
 #get signles and multiples:
 
-blank_list = []
-with open(tsv_cluster, 'r') as clustermap:
-    for entry in clustermap:
-        representative = entry.split('\t')[0]
-        blank_list.append(representative)
 
-clustermap.close()
+Hit_list = []
+for record in SeqIO.parse(Arc_fasta, 'fasta'):
+    Hit_list.append(record.id)
+Tax_dict = {}
+import os
+for entry in os.listdir('/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Archaea/cross_domain'):
+    if 'faa.fasta' in entry:
+        print(entry)
+        for record in SeqIO.parse('/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Archaea/cross_domain/{}'.format(entry), 'fasta'):
+            Tax_dict[record.id.split('_tax:')[0]] = record.id.split('tax:d__')[1]
+        
+        
 
 from collections import Counter
-import json
+
+rep_list = (rep_list)
+reps = Counter(rep_list)
 
 singletonlist = []
 multiplelist = []
-blank = Counter(blank_list)
-items = blank.items()
+items = reps.items()
+
 for i in items:
     if i[-1] == 1:
         singletonlist.append(i[0])
@@ -57,10 +62,7 @@ with open(tsv_cluster, 'r') as clustermap:
         #latest element to check in, drop count = 1 line as in multiple list does the same
 import json
 def get_taxonomy_cluster(cluster_rep):
-    with open(diction) as json_file:
-        data = json.load(json_file)
-
-
+    
     entry = Rep_dict[cluster_rep]
     num_proteins = (len(entry))
     species_list = []
@@ -70,12 +72,12 @@ def get_taxonomy_cluster(cluster_rep):
     class_list = []
     phylum_list = []
     for prot_id in entry:
-        species_list.append(data[prot_id][-1].split(';s__')[1])
-        genus_list.append(data[prot_id][-1].split(';g__')[1].split(';s')[0])
-        family_list.append(data[prot_id][-1].split(';f__')[1].split(';g')[0])
-        order_list.append(data[prot_id][-1].split(';o__')[1].split(';f')[0])
-        class_list.append(data[prot_id][-1].split(';c__')[1].split(';o')[0])
-        phylum_list.append(data[prot_id][-1].split(';p__')[1].split(';c')[0])
+        species_list.append(Tax_dict[prot_id][-1].split(';s__')[1])
+        genus_list.append(Tax_dict[prot_id][-1].split(';g__')[1].split(';s')[0])
+        family_list.append(Tax_dict[prot_id][-1].split(';f__')[1].split(';g')[0])
+        order_list.append(Tax_dict[prot_id][-1].split(';o__')[1].split(';f')[0])
+        class_list.append(Tax_dict[prot_id][-1].split(';c__')[1].split(';o')[0])
+        phylum_list.append(Tax_dict[prot_id][-1].split(';p__')[1].split(';c')[0])
     species_list = list(set(species_list))
     genus_list = list(set(genus_list))
     family_list = list(set(family_list))
@@ -90,7 +92,29 @@ def get_taxonomy_cluster(cluster_rep):
     lc = len(class_list)
     lp = len(phylum_list)
     string = 'num proteins: {}, numb uniq s: {}, num uniq g: {},  number of uniq f: {},  num uniq o: {}, num uniq c: {}, num uniq p {}'.format(num_proteins, ls, lg, lf, lo, lc, lp)
-    if lf > 1:
-        return(num_proteins)
-    else:
-        return(0)
+    return(string)
+
+
+
+nclusters = 0
+nproteins =  0
+phyla_list = []
+for cluster_rep in Rep_dict.keys():
+    clusteroid = (get_taxonomy_cluster(cluster_rep))
+    if clusteroid[-5] > 1:
+        if clusteroid[-4] > 1:
+            pass
+        else:
+            
+            nclusters = nclusters + 1
+    
+            print('\n' + 'taxa of cluster seqs with more than one order, with rep {}'.format(cluster_rep))
+            nproteins = nproteins + clusteroid[0] 
+            entry = Rep_dict[cluster_rep]
+            for prot_id in entry:
+               print(Tax_dict[prot_id])
+               phyla_list.append(Tax_dict[prot_id].split('f__')[1].split(';g')[0])
+                
+print(nclusters)
+print(nproteins)
+print(Counter(phyla_list))
