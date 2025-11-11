bf1a98e40219283087452839cf844f26169ec3f2
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-11-11 16:21:36 +1300

    Update and rename call_red_clades_(inprogress)(t4r).py to call_red_clades(t4r).py

--- tree_generation/call_red_clades(t4r).py
+++ tree_generation/call_red_clades(t4r).py
@@ -8,7 +8,7 @@ from Bio.Phylo.Consensus import *
 #redo the clade name as it was done for the tree in previous code so we are uniform and consistent
 
         
-tree = Phylo.read('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/CPA_phylogeny_midroot_TREE_ALIGNEDRED_7Oktsept_ft.nw', 'newick')
+tree = Phylo.read('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/CPA_phylogeny_midroot_TREE_ALIGNEDRED_7Nov_ft_lg_cat_gamma.nw', 'newick')
 count = 0
 #only give clade a number/name if it is not terminal
 # otherwise let it keep the same name, this prevents future back paddeling and renaming. 
@@ -27,7 +27,7 @@ for clade in tree.find_clades():
 #get the reps and their matching rep value
 
 Red_dict = {}
-with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/RED_info_CPA_phylogeny_FTtree_7oct.tsv', 'r') as clade_inf:
+with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/info_RED_clades_CPA_phylogeny_FTtree_lg_cat_gamma_7Nov.tsv', 'r') as clade_inf:
     next(clade_inf, None)
     for inf in clade_inf:
         RED =(inf.split('\t')[5])
@@ -40,75 +40,76 @@ print(len(Red_dict.keys()))
 for clade in tree.find_clades():
     entry = str(clade.name)
     clade.RED = float(Red_dict[entry])
-def get_subset_inlcuding_terminal(tree,lowerlimit,upperlimit):
+def get_subset_inlcuding_terminal(tree,lowerlimit,upperlimit, clade_2_exclude):
     parent_list = []
     skipped_list = []
     for clade in tree.find_clades():
         if clade != tree.root:
-
-            #perform actions for terminal nodes
-            if clade.is_terminal() == True:
-               #in this case the clade is terminal
-
-                if len(parent_list) == 0:
-                    parent_list.append(clade)
-            
-                else:
-                    path_list = tree.get_path(clade)
-                                        #this flag allows me to see if I am not redoing trees which already contain certain clades. 
-                    path_already_done = False
-                        
-                    for x in parent_list:
-                       if x in path_list:
-                           path_already_done = True
-                    if path_already_done == True:
-                        pass
-                    else:
+            if str(clade.name) != str(clade_2_exclude):
+                #perform actions for terminal nodes
+                if clade.is_terminal() == True:
+                   #in this case the clade is terminal
+    
+                    if len(parent_list) == 0:
                         parent_list.append(clade)
-
-
-            else:
-                if clade.is_preterminal() == True:
-                    if len(clade.get_terminals()) == 2:
-                        length_leaves = 0
-                        for leaves in clade.get_terminals():
-                            length_leaves += leaves.branch_length
-                        if length_leaves == 0:
-                            continue
-            
-                if clade.RED > lowerlimit:
-                    if clade.RED < upperlimit:
-                            # we keep running into an issue were clade Bac226_18731726 keeps being labbeled as a non terminal node, and it has a branch_length of 0.38 while having a RED value of 1.
-                            #something which
-                            if type(clade.confidence) != float:
+                
+                    else:
+                        path_list = tree.get_path(clade)
+                                            #this flag allows me to see if I am not redoing trees which already contain certain clades. 
+                        path_already_done = False
+                            
+                        for x in parent_list:
+                           if x in path_list:
+                               path_already_done = True
+                        if path_already_done == True:
+                            pass
+                        else:
+                            parent_list.append(clade)
+    
+    
+                else:
+                    if clade.is_preterminal() == True:
+                        if len(clade.get_terminals()) == 2:
+                            length_leaves = 0
+                            for leaves in clade.get_terminals():
+                                length_leaves += leaves.branch_length
+                            if length_leaves == 0:
                                 continue
-                            if clade.confidence > 0.94:
-                                if clade.name not in parent_list:
-                                    if len(parent_list) == 0:
-                                        parent_list.append(clade)
-            
-                                    else:
-                                        path_list = tree.get_path(clade)
-                                        #this flag allows me to see if I am not redoing trees which already contain certain clades. 
-                                        path_already_done = False
-                        
-                                        for x in parent_list:
-                                            if x in path_list:
-                                                path_already_done = True
-                                        if path_already_done == True:
-                                            pass
-                                        else:
+                
+                    if clade.RED > lowerlimit:
+                        if clade.RED < upperlimit:
+                                # we keep running into an issue were clade Bac226_18731726 keeps being labbeled as a non terminal node, and it has a branch_length of 0.38 while having a RED value of 1.
+                                #something which
+                                if type(clade.confidence) != float:
+                                    continue
+                                if clade.confidence > 0.94:
+                                    if clade.name not in parent_list:
+                                        if len(parent_list) == 0:
                                             parent_list.append(clade)
-                    
+                
+                                        else:
+                                            path_list = tree.get_path(clade)
+                                            #this flag allows me to see if I am not redoing trees which already contain certain clades. 
+                                            path_already_done = False
+                            
+                                            for x in parent_list:
+                                                if x in path_list:
+                                                    path_already_done = True
+                                            if path_already_done == True:
+                                                pass
+                                            else:
+                                                parent_list.append(clade)
                         
-
+                            
+            else: 
+                pass
 
     return(parent_list)
 
 
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.225, 0.5)
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.99, 27958)
 print('genus')
 node_dic7 = {}
 print(len(parent_list))
@@ -124,7 +125,7 @@ genus_list = []
 genus = parent_list
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.24,0.4275)
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.265, 27958)
 print('family')
 node_dic6 = {}
 print(len(parent_list))
@@ -140,7 +141,7 @@ family_list =  parent_list
 print(len(family_list))
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.24, 0.47)
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.28, 27958)
 print('order')
 node_dic5 = {}
 print(len(parent_list))
@@ -156,7 +157,7 @@ order_list =  parent_list
 print(len(order_list))
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.225, 0.4275)
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.292, 27958)
 print('class')
 node_dic4 = {}
 print(len(parent_list))
@@ -172,8 +173,8 @@ class_list =  parent_list
 print(len(class_list))
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.1991,0.4275)
-print('Phylum1')
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.31, 27958)
+print('Phylum2')
 node_dic3 = {}
 print(len(parent_list))
 for entry in parent_list:
@@ -188,8 +189,8 @@ Phylum1_list =  parent_list
 print(len(Phylum1_list))
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0,0.225)
-print('Phylum2')
+parent_list = get_subset_inlcuding_terminal(tree, 0.07, 0.32, 27958)
+print('Phylum1')
 node_dic2 = {}
 print(len(parent_list))
 for entry in parent_list:
@@ -204,7 +205,7 @@ Phylum2_list =  parent_list
 print(len(Phylum2_list))
 
 parent_list =[]
-parent_list = get_subset_inlcuding_terminal(tree, 0.0,0.24)
+parent_list = get_subset_inlcuding_terminal(tree,0.07, 0.35, 27958)
 print('Domain')
 node_dic1 = {}
 print(len(parent_list))
@@ -220,15 +221,20 @@ Domain_list = []
 Domain_list =  parent_list
 print(len(Domain_list))
 
-with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/Red_informed_clade_bigfamily_3nov.tsv', 'w') as Big:
+with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/Red_informed_clade_bigfamily_12nov.tsv', 'w') as Big:
     header = 'protein_id' + '\t' + 'Domain' + '\t' + 'Phyla1' + '\t' + 'Phyla2' + '\t' + 'Class' + '\t' + 'Order' + '\t' + 'Family' + '\t' + 'Genus' + '\n'
     Big.write(header)
 
     for clade in tree.find_clades():
         if clade != tree.root:
             if clade.is_terminal() == True:
-                if len(clade.name) > 37:
+                if 'Homo' in clade.name:
+                    clade.temp = clade.name
+                elif 'Mus' in clade.name:
+                    clade.temp = clade.name
+                elif len(clade.name) > 37:
                     clade.temp = short_dict[clade.name]
+
                 else:
                     clade.temp = clade.name
                 if str(clade.temp) in node_dic1.keys():
