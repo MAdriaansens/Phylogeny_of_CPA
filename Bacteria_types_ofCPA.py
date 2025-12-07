483b36f346c547a36e5e718e5832e10cbe4028ac
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-12-07 15:33:47 +1300

    Update Bacteria_types_ofCPA.py

--- Bacteria_types_ofCPA.py
+++ Bacteria_types_ofCPA.py
@@ -2,13 +2,12 @@ import json
 
 clade_dic = {}
 
-with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/Red_informed_clade_bigfamily_12nov_lgcatgamma.tsv', 'r') as LG_Cat_Gamma_clade:
+with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/Red_informed_clade_20_lg_cat_gamma_RED_interval.tsv', 'r') as LG_Cat_Gamma_clade:
     next(LG_Cat_Gamma_clade, None)
     for line in LG_Cat_Gamma_clade:
-        if 'Bac226' in line:
-            protein_id = line.split('\t')[0]
-            domain = line.split('\t')[1]
-            clade_dic[protein_id] = domain
+        protein_id = line.split('\t')[0]
+        domain = line.split('\t')[1]
+        clade_dic[protein_id] = domain
 domain_list = []
 for key in clade_dic.keys():
     domain_list.append(clade_dic[key])
@@ -36,33 +35,25 @@ Tax_list = set(Tax_list)
 print(len(Tax_list))
 print(len(Tax_dic.keys()))
 
-#not the problem
 Reps_dic = {}
-count = 0
 with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/RED_values_Bacteria_clusters_22sep.tsv', 'r') as Reps_Bac:
     next(Reps_Bac, None)
     for line in Reps_Bac:
-        Rep = (line.split('\t')[1]).replace('"', '').replace('"', '')
-        Clusteroids = (line.split('\t')[15])
+        Rep = (line.split('\t')[0])
+        Clusteroids = (line.split('\t')[-3])
         Reps_dic[Rep] = Clusteroids
-
 
 CPA1_dict = {}
 CPA2_dict = {}
 Kef_dict = {}
 NhaA_dict = {}
+Lates_dict={}
 Uncharacterized  = {}
-
-print(len(Reps_dic))
-for key in Reps_dic.keys():
-    print(key)
-    break
-#in bac this should be 27414, only uses the representatives not the singletons
-
-
-
-
-
+DxK_pseudo_dict = {}
+NxKGamma_dict = {}
+NhaS5_dict = {}
+UncArc_dict = {}
+UncProk_dict = {}
 
 
 for key in clade_dic.keys():
@@ -73,11 +64,10 @@ for key in clade_dic.keys():
             if clade_dic[key] == '35427':
                 #now we unpack the representatives
                 if key in Reps_dic:
-
+                    
                     for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
                         CPA1_dict[entry] = 'CPA1'
                 else:
-
                     CPA1_dict[key] = 'CPA1'
                 
     
@@ -105,6 +95,55 @@ for key in clade_dic.keys():
                         NhaA_dict[entry] = 'NhaA'
                 else:
                     NhaA_dict[key] = 'NhaA'
+            elif clade_dic[key] == '58708':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        Lates_dict[entry] = 'Latescibacteriota_cpa'
+                else:
+                    Lates_dict[key] = 'Latescibacteriota_cpa'
+
+            elif clade_dic[key] == '58745':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        DxK_pseudo_dict[entry] = 'DxK_Pseudomonadota'
+                else:
+                    DxK_pseudo_dict[key] = 'DxK_Pseudomonadota'
+            elif clade_dic[key] == '59314':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        NxKGamma_dict[entry] = 'NxK_Gammaproteobacteriota'
+                else:
+                    NxKGamma_dict[key] = 'NxK_Gammaproteobacteriota'
+            elif clade_dic[key] == '65825':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        NhaS5_dict[entry] = 'NhS5'
+                else:
+                    NhaS5_dict[key] = 'NhaS5'
+            elif clade_dic[key] == '65668':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        UncArc_dict[entry] = 'UncArc'
+                else:
+                    UncArc_dict[key] = 'Uncharcaterized_Archaea'
+            elif clade_dic[key] == '61608':
+                                                            #now we unpack the representatives
+                if key in Reps_dic:
+
+                    for entry in Reps_dic[key].replace("[","").replace("]", "").replace("'", "").replace(" ", "").split(","):
+                        UncPro_dict[entry] = 'Uncharacterized_Prokarya'
+                else:
+                    UncPro_dict[key] = 'Uncharcaterized_Prokarya'
             else:
                                                             #now we unpack the representatives
                 if key in Reps_dic:
@@ -115,21 +154,16 @@ for key in clade_dic.keys():
                     Uncharacterized[key] = 'Unc'
             
 print(len(set(CPA1_dict.keys())))
-
-
 print(len(set(CPA2_dict.keys())))  
 print(len(set(Kef_dict.keys())))        
 print(len(set(NhaA_dict.keys())))
 print(len(set(Uncharacterized.keys())))
-
-
     
-#make sure this some is similar to grep -c '>' Archaea input file
+#make sure this some is similar to grep -c '>' Archaea input filei
 
 CPA1_list_tax = []
 CPA1_tax_id = []
 for key in CPA1_dict.keys():
-    key =(key.replace('"', ''))
     CPA1_list_tax.append(Tax_dic[key])
     #WORK ON THIS AFTER NZMS
     #info_list = []
@@ -140,43 +174,63 @@ print(CPA1_list_tax[1])
 
 CPA2_list_tax = []
 for key in CPA2_dict.keys():
-    key =(key.replace('"', ''))
-
     CPA2_list_tax.append(Tax_dic[key])
 print(len(CPA2_list_tax))
 
 NhaA_list_tax = []
 for key in NhaA_dict.keys():
-    key =(key.replace('"', ''))
-
     NhaA_list_tax.append(Tax_dic[key])
 print(len(NhaA_list_tax))
 
 Kef_list_tax = []
 for key in Kef_dict.keys():
-    key =(key.replace('"', ''))
-
     Kef_list_tax.append(Tax_dic[key])
 print(len(Kef_list_tax))
 
 
 Unc_list_tax = []
 for key in Uncharacterized.keys():
-    key =(key.replace('"', ''))
-
     Unc_list_tax.append(Tax_dic[key])
 print(len(Unc_list_tax))
 
+Lates_list_tax = []
+for key in Lates_dict.keys():
+    Lates_list_tax.append(Tax_dic[key])
+print(len(Lates_list_tax))
 
+
+DxK_pseudo_list_tax = []
+for key in DxK_pseudo_dict.keys():
+    DxK_pseudo_list_tax.append(Tax_dic[key])
+print(len(DxK_pseudo_list_tax))
+
+NxKGamma_list_tax = []
+for key in NxKGamma_dict.keys():
+    NxKGamma_list_tax.append(Tax_dic[key])
+print(len(NxKGamma_list_tax))
+
+
+NhaS5_list_tax = []
+for key in NhaS5_dict.keys():
+    NhaS5_list_tax.append(Tax_dic[key])
+print(len(NhaS5_list_tax))
+
+UncArc_list_tax = []
+for key in UncArc_dict.keys():
+    UncArc_list_tax.append(Tax_dic[key])
+print(len(UncArc_list_tax))
+
+UncProk_list_tax = []
+for key in UncProk_dict.keys():
+    UncProk_list_tax.append(Tax_dic[key])
+print(len(UncProk_list_tax))
 
 
 with open('/nesi/nobackup/uc04105/new_databases_May/final_tree_set/Types_CPA_in_BacteriaGTDB226.tsv', 'w') as TAX_TYPE:
-    header = 'GTDB_id' + '\t' + 'Uncharcterized' + '\t' + 'Kef' + '\t' + 'CPA1' + '\t' + 'CPA2' + '\t' + 'NhaA' + '\n'
+    header = 'GTDB_id' + '\t' + 'Uncharcterized' + '\t' + 'Kef' + '\t' + 'CPA1' + '\t' + 'CPA2' + '\t' + 'NhaA' + '\t' + 'NhaS5' + '\t' + 'Uncharacterized_Archaea' + '\t' + 'Uncharacterized_prokarya' + '\t' + 'DxK_Pseudomonadota' + '\t' + 'NxK_Gammaproteobacteriota' + '\t' + 'Latescibacteriota' + '\n'
     TAX_TYPE.write(header)
     total_count = 0
     for tax in Tax_list:
         GTDB_tax = tax
-        line = GTDB_tax + '\t' + str(Unc_list_tax.count(GTDB_tax)) + '\t' + str(Kef_list_tax.count(GTDB_tax)) + '\t' + str(CPA1_list_tax.count(GTDB_tax)) + '\t' +  str(CPA2_list_tax.count(GTDB_tax)) + '\t' + str(NhaA_list_tax.count(GTDB_tax)) + '\n'
-        total_count = total_count + Unc_list_tax.count(GTDB_tax) + Kef_list_tax.count(GTDB_tax) + CPA1_list_tax.count(GTDB_tax) + CPA2_list_tax.count(GTDB_tax) + NhaA_list_tax.count(GTDB_tax)
+        line = GTDB_tax + '\t' + str(Unc_list_tax.count(GTDB_tax)) + '\t' + str(Kef_list_tax.count(GTDB_tax)) + '\t' + str(CPA1_list_tax.count(GTDB_tax)) + '\t' +  str(CPA2_list_tax.count(GTDB_tax)) + '\t' + str(NhaA_list_tax.count(GTDB_tax)) + '\t'+ str(NhaS5_list_tax.count(GTDB_tax)) + '\t' +  str(UncProk_tax.count(GTDB_tax)) + '\t' +  str(UncArc_list_tax.count(GTDB_tax)) + '\t' +str(UncProk_list_tax.count(GTDB_tax)) + '\t' +  str(DxK_pseudo_list_tax.count(GTDB_tax)) + '\t' +  str(NxKGamma_list_tax.count(GTDB_tax)) + '\t' + str(Lates_list_tax.count(GTDB_tax)) + '\n'
         TAX_TYPE.write(line)
-print(total_count)
