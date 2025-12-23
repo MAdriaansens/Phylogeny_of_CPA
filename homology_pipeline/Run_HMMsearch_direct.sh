cef22009fe7e2f7ba73aa19bf11f48f388496112
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2025-12-23 13:08:21 +1300

    Update Run_HMMsearch_direct.sh

--- homology_pipeline/Run_HMMsearch_direct.sh
+++ homology_pipeline/Run_HMMsearch_direct.sh
@@ -21,15 +21,10 @@ module load Python/3.11.6-foss-2023a
 module load HMMER/3.3.2-GCC-12.3.0
 
 #07
-#hmmsearch --noali --cpu 10 -E 0.0000001 --tblout ${HMMsearch}/PF00999_07_vsEuk.tsv ${HMMdir}/PF00999.hmmm
- ${Euk_DB}
+hmmsearch --noali --cpu 10 -E 0.0000001 --tblout ${HMMsearch}/PF00999_07_vsEuk.tsv ${HMMdir}/PF00999.hmmm ${Euk_DB}
 
-#python getting_fasta_from_hit_extra.py ${HMMsearch}/PF00999_07_vsEuk.tsv HMM ${Euk_TSV} ${HMMsearch}/PF00
-0999_07_vsEuk.fasta EukM6
+python getting_fasta_from_hit_extra.py ${HMMsearch}/PF00999_07_vsEuk.tsv HMM ${Euk_TSV} ${HMMsearch}/PF000999_07_vsEuk.fasta EukM6
 
-#hmmalign --amino --trim -o /nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMalign/PFF
-00999_07_vsEuk_AlignedPF00999.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm /nesi/nobackup/uc04105//
-new_databases_May/Euk_database_May/results/HMMsearch/PF00999_07_vsEuk.fasta
+#hmmalign --amino --trim -o /nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMalign/PF00999_07_vsEuk_AlignedPF00999.sthk /nesi/nobackup/uc04105/results/HMM/PF00999.hmm /nesi/nobackup/uc04105//new_databases_May/Euk_database_May/results/HMMsearch/PF00999_07_vsEuk.fasta
 
-python parse_stockholm_filter.py ${HMMalign}/PF00999_07_vsEuk_AlignedPF00999.sthk ${HMMalign}/PF00999_07__
-vsEuk_AlignedPF00999 257
+python parse_stockholm_filter.py ${HMMalign}/PF00999_07_vsEuk_AlignedPF00999.sthk ${HMMalign}/PF00999_07__vsEuk_AlignedPF00999 257
