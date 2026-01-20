7ddc743dedfdfd9cb808c34190694d93e1622981
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-01-20 14:39:50 +1300

    Update Parse_model_selection.py

--- tree_generation/auxillary/Parse_model_selection.py
+++ tree_generation/auxillary/Parse_model_selection.py
@@ -1,115 +1,78 @@
-Euk_dir='/nesi/nobackup/uc04105/new_databases_May/Euk_database_May/results/HMMalign/random_list'
+import os
 
-Arc_dir='/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Archaea/intermediate_PF00999/random_list'
+Arc_dir = '/nesi/nobackup/uc04105/Likelihood_CPA/Archaea'
+Bac_dir = '/nesi/nobackup/uc04105/Likelihood_CPA/Bacteria'
+Euk_dir = '/nesi/nobackup/uc04105/Likelihood_CPA/Eukarya'
+Seed_dir = '/nesi/nobackup/uc04105/Likelihood_CPA/Seeds'
 
-Bac_dir ='/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Bacteria/PF00999/MERGED/random_list'
+AIK_Arc = '{}/AIK_Arc.txt'.format(Arc_dir)
+BIC_Arc = '{}/BIC_Arc.txt'.format(Arc_dir)
 
-Seeds='/nesi/nobackup/uc04105/fasta_files/Seed_NhaA/randomList'
+AIC_dic = {}
+BIC_dic = {}
 
-likelihood ='Seed_likelihood.txt'
-model='Seeds_models.txt'
+#we make a dic matching the model chosen for each number of subset of sequences 
 
-#each text file was generated using the grep 'Best-fit model:' subset_*/* and grep 'BEST SCORE FOUND' subset_*/* commands
+with open(AIK_Arc, 'r') as AIK:
+    for line in AIK:
+        model_chosen = line.split(': ')[-1].split(' ')[0]
+        number_of_file = line.split('.fasta.log')[0].split('_')[-1]
+        AIC_dic[number_of_file] = model_chosen
+        
 
-model_dict = {}
-score_dict = {} 
+with open(BIC_Arc, 'r') as BIC:
+    for line in BIC:
+        model_chosen = line.split(': ')[-1].split(' ')[0]
+        number_of_file = line.split('.fasta.log')[0].split('_')[-1]
+        BIC_dic[number_of_file] = model_chosen
 
-with open('{}/{}'.format(Seeds, model), 'r') as Mod:
-    for line in Mod:
-        numb =int((line.split('_')[1].split('/')[0]))
-        model_chosen = (line.split(' chosen')[0].split(': ')[1])
-        model_dict[numb] = model_chosen
-with open('{}/{}'.format(Seeds, likelihood), 'r') as Like:
-    for line in Like:
-        numb =int((line.split('_')[1].split('/')[0]))
-        score =(line.split('\n')[0].split(': ')[1])
-        score_dict[numb] = score
+count = 0
 
-total_dict = {}
-for x in range(1,101):
-    tlist = []
-    model = model_dict[x]
-    score = score_dict[x]
-    name = 'Seed_{}'.format(x)
-    tlist.append(model)
-    tlist.append(score)
-    total_dict[name] = tlist
-score_Adict = {}
-model_Adict = {}
-
-likelihoodA = 'Archaea_likelihood.txt'
-modelA='Archaea_tree_model.txt'
-
-with open('{}/{}'.format(Arc_dir, modelA), 'r') as Mod:
-    for line in Mod:
-        numb =int((line.split('_')[1].split('/')[0]))
-        model_chosen = (line.split(' chosen')[0].split(': ')[1])
-        model_Adict[numb] = model_chosen
-with open('{}/{}'.format(Arc_dir, likelihoodA), 'r') as Like:
-    for line in Like:
-        numb =int((line.split('_')[1].split('/')[0]))
-        score =(line.split('\n')[0].split(': ')[1])
-        score_Adict[numb] = score
-
-for x in range(1,101):
-    tlist = []
-    model = model_Adict[x]
-    score = score_Adict[x]
-    name = 'Arc_{}'.format(x)
-    tlist.append(model)
-    tlist.append(score)
-    total_dict[name] = tlist
-  score_Bdict = {}
-model_Bdict = {}
-
-likelihoodB ='Best_score.txt'
-modelB= 'Bacteria_hmmscanned_model.txt'
-
-with open('{}/{}'.format(Bac_dir, modelB), 'r') as Mod:
-    for line in Mod:
-        numb =int((line.split('_')[1].split('/')[0]))
-        model_chosen = (line.split(' chosen')[0].split(': ')[1])
-        model_Bdict[numb] = model_chosen
-with open('{}/{}'.format(Bac_dir, likelihoodB), 'r') as Like:
-    for line in Like:
-        numb =int((line.split('_')[1].split('/')[0]))
-        score =(line.split('\n')[0].split(': ')[1])
-        score_Bdict[numb] = score
-
-for x in range(1,101):
-    tlist = []
-    model = model_Bdict[x]
-    score = score_Bdict[x]
-    name = 'Bac_{}'.format(x)
-    tlist.append(model)
-    tlist.append(score)
-    total_dict[name] = tlist
-score_Edict = {}
-model_Edict = {}
-
-likelihoodE ='Eukarya_likelihood.txt'
-modelE= 'Model_picked_euk_pf00999.txt'
-
-with open('{}/{}'.format(Euk_dir, modelE), 'r') as Mod:
-    for line in Mod:
-        numb =int((line.split('_')[1].split('/')[0]))
-        model_chosen = (line.split(' chosen')[0].split(': ')[1])
-        model_Edict[numb] = model_chosen
-with open('{}/{}'.format(Euk_dir, likelihoodE), 'r') as Like:
-    for line in Like:
-        numb =int((line.split('_')[1].split('/')[0]))
-        score =(line.split('\n')[0].split(': ')[1])
-        score_Edict[numb] = score
-
-for x in range(1,101):
-    tlist = []
-    model = model_Edict[x]
-    score = score_Edict[x]
-    name = 'Euk_{}'.format(x)
-    tlist.append(model)
-    tlist.append(score)
-    total_dict[name] = tlist
-
-
-print(len(list(total_dict.keys())))
-#should total 400. 
+with open('/nesi/nobackup/uc04105/Likelihood_CPA/Archaea/Arc_likelihood_CPA_random100.tsv', 'w') as random100:
+    header = 'random_set_number' + '\t'  + 'AIC_model' + '\t' + 'AIC_score' + '\t' + 'df_A' + '\t' +  + 'BIC_model' + '\t' + 'BIC_score' + '\t' + 'df_B' + '\n'
+    random100.write(header)
+    for file in os.listdir(Arc_dir):
+        if file.split('.')[-1] == 'log':
+            logfile = file
+            #now we retrieve the best model chosen by AIC and BIC
+            number_of_file =file.split('.fasta.log')[0].split('_')[-1]
+            AIC_best_model = AIC_dic[number_of_file]
+                    #now we retrieve the best model chosen by AIC and BIC
+            BIC_best_model = BIC_dic[number_of_file]
+            with open('{}/{}'.format(Arc_dir, logfile), 'r') as logout:
+                for row in logout:
+                    #check for the line with our best model
+                    if AIC_best_model in row:
+                        #this is to make sure that we are in model finder
+                        if row[1].isnumeric():
+                            list_row = row.split(' ')
+                            #there are multiple blank spaces in this
+                            for entry in list_row:
+                                if entry == '':
+                                    list_row.remove(entry)
+                            for entry in list_row:
+                                if entry == '':
+                                    list_row.remove(entry)
+                            
+                            df_A = list_row[3]
+                            AIC_score = list_row[4]
+                            
+                        #modelfinder has now started
+                    #repeat same for BIC
+                    if BIC_best_model in row:
+                        #this is to make sure that we are in model finder
+                        if row[1].isnumeric():
+                            list_row = row.split(' ')
+                            #there are multiple blank spaces in this
+                            for entry in list_row:
+                                if entry == '':
+                                    list_row.remove(entry)
+                            for entry in list_row:
+                                if entry == '':
+                                    list_row.remove(entry)
+                            df_B = list_row[3]
+                            BIC_score = list_row[-1].split('\n')[0]
+            count = count + 1
+            outline = number_of_file + '\t' +  AIC_best_model + '\t' +  AIC_score + '\t' +  df_A + '\t' +  BIC_best_model + '\t' +  BIC_score + '\t' +  df_B + '\n'
+            Random100.write(outline)
+                        #modelfinder has now started
