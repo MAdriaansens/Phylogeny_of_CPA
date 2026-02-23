da36bc42ec6d8db5ea80462c5878f4da2359cc60
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-02-23 13:39:32 +1300

    Update Run_perl_in_bash.sh

--- Step2_In-house-HMM/Pfam/Run_perl_in_bash.sh
+++ Step2_In-house-HMM/Pfam/Run_perl_in_bash.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=perl
+#SBATCH --job-name=perl03600
 #SBATCH --time=48:00:00      # Walltime (HH:MM:SS
 #SBATCH --mem=9000MB          # Memory in MB
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=5
 #SBATCH --account=uc04105 
-#SBATCH --output=slurm_output/perl03553_output%A.out
-#SBATCH --error=slurm_output/perl03553_errror%A.err
+#SBATCH --output=slurm_output/perl03600_output%A.out
+#SBATCH --error=slurm_output/perl03600_errror%A.err
 
 perl download_PF03600.pl
+#the script outputs the fasta sequences into the .out file. 
