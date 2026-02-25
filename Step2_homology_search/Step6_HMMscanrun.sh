25cc3c28b695c107e04f736918a34a5e00d1d07b
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-02-25 14:49:48 +1300

    Update Step6_HMMscanrun.sh

--- Step2_homology_search/Step6_HMMscanrun.sh
+++ Step2_homology_search/Step6_HMMscanrun.sh
@@ -21,9 +21,10 @@ module load HMMER/3.3.2-GCC-12.3.0
 HMMDB=/nesi/nobackup/uc04105/results/HMM/Pfam_db/Merged_PFAMA.hmm
 module load HMMER/3.3.2-GCC-12.3.0
 
-python parse_stockholm_special.py ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 212
-python parse_stockholm_special.py ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 235
-python parse_stockholm_special.py ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 361
+python parse_stockholm_forHMMscan.py ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 212
+python parse_stockholm_forHMMscan.py ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 235
+python parse_stockholm_forHMMscan.py ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 361
+
 
 hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF06450/PF06450_Bacteria_merged_alignedPF06450_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.fasta
 
