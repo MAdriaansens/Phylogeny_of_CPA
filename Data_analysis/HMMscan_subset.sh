bc88033a9ee29736db67f8944845e1579ac570a1
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-02-25 14:23:22 +1300

    Update HMMscan_subset.sh

--- Data_analysis/HMMscan_subset.sh
+++ Data_analysis/HMMscan_subset.sh
@@ -21,12 +21,12 @@ module load HMMER/3.3.2-GCC-12.3.0
 HMMDB=/nesi/nobackup/uc04105/results/HMM/Pfam_db/Merged_PFAMA.hmm
 module load HMMER/3.3.2-GCC-12.3.0
 
-#python parse_stockholm_special.py ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter 212
-#python parse_stockholm_special.py ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter 235
-#python parse_stockholm_special.py ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter 361
+python parse_stockholm_special.py ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 212
+python parse_stockholm_special.py ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 235
+python parse_stockholm_special.py ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.sthk  ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]} 361
 
-hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF06450/PF06450_Bacteria_merged_alignedPF06450_refilter_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter.fasta
+hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF06450/PF06450_Bacteria_merged_alignedPF06450_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF06450/cross_domain/PF06450_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.fasta
 
-#hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF03553/PF03553_Bacteroa_merged_alignedPF03553_refilter_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter.fasta
+hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF03553/PF03553_Bacteria_merged_alignedPF03553_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF03553/cross_domain/PF03553_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.fasta
 
-#hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF03600/PF03600_Bacteroamerged_alignedPF03600_refilter_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}_refilter.fasta
+hmmscan -E 0.001 --cpu 10 --tblout ${HMMscan}/PF03600/PF03600_Bacteriamerged_alignedPF03600_${array[$SLURM_ARRAY_TASK_ID]}HMMscanned.tsv ${HMMDB} ${HMMalign}/PF03600/cross_domain/PF03600_Bacteria_crossdomain_searched_all_${array[$SLURM_ARRAY_TASK_ID]}.fasta
