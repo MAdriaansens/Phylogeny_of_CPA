98ddcf8419fc91723eebd583ec9c9d6e6ccece30
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-03-09 15:49:26 +1300

    Update Create_TSV_and_proteinDB_from_GTDB_data1.py

--- Step1_Database_creation/GTDB/Create_TSV_and_proteinDB_from_GTDB_data1.py
+++ Step1_Database_creation/GTDB/Create_TSV_and_proteinDB_from_GTDB_data1.py
@@ -15,7 +15,7 @@ dictionary = {}
 
 with open('{}'.format(tsv_file)) as dataset:
     for i in dataset:
-        #this is the GTDB_id
+        #this is the GTDB_id of representative of set genome
         if i.split('\t')[17] != 'gtdb_genome_representative':
             dictionary[(i.split('\t')[17])] = (i.split('\t')[19]).replace(' ', '_')
 
