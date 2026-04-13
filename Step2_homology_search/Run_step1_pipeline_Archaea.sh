ae6d3001b9a61e7aff7b16393745711f982b8423
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-04-14 08:36:20 +1200

    Update Run_step1_pipeline_Archaea.sh

--- Step2_homology_search/Run_step1_pipeline_Archaea.sh
+++ Step2_homology_search/Run_step1_pipeline_Archaea.sh
@@ -20,41 +20,39 @@ Seq=/nesi/nobackup/uc04105/cross_biome_metagenome/Protein/sequences
 #---------------------CPA-------------------------
 module load MMseqs2/15-6f452-gompi-2023a
 
-mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 20 ${Seq} ${Arc_db} ${MMseqs}/PF00999seq_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv ${array[$SLURM_ARRAY_TASK_ID]}_Arc_tmp
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 20 ${Seq}  ${Bac_db} ${MMseqs}/PF00999seq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
 
 module load Python/3.11.6-foss-2023a
-python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00999seq_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00999seq_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00999seq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF00999seq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
 
 module purge
 
 module load HMMER/3.4-GCC-12.3.0
 
 #HMMsearch
-hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999hmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Arc_db}
+hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999hmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Bac_db}
 
 module load Python/3.11.6-foss-2023a
-python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999hmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Arc_TSV} ${HMMsearch}/PF00999hmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
+python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999hmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Bac_TSV} ${HMMsearch}/PF00999hmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
 
 HMMdir=/home/mad149/00_nesi_projects/uc04105_nobackup/cross_biome_metagenome/Protein/HMM/Archaea_Manual_e5_cov50_AlignedPF00999_clustered_0.6_rep_seq_Ginsialigned.hmm
-
-hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Archmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Arc_db}
+hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Archmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Bac_db}
 
 module load Python/3.11.6-foss-2023a
-python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Archmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Arc_TSV} ${HMMsearch}/PF00999Archmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
+python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Archmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Bac_TSV} ${HMMsearch}/PF00999Archmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
 
 HMMdir=/home/mad149/00_nesi_projects/uc04105_nobackup/cross_biome_metagenome/Protein/HMM/Manual_seq_cov30_e05_seqid0.7_genafpair_aligned.hmm
-hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Eukchmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Arc_db}
+hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Eukhmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Bac_db}
 
 module load Python/3.11.6-foss-2023a
-python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Eukchmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Arc_TSV} ${HMMsearch}/PF00999Eukhmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
+python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Eukhmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Bac_TSV} ${HMMsearch}/PF00999Eukhmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
 
 
 HMMdir=/home/mad149/00_nesi_projects/uc04105_nobackup/cross_biome_metagenome/Protein/HMM/Manual_vsBacteria_merged_e5_cov30_seqid0.6.faa_rep_seq_autoaligned.hmm
-hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Bachmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Arc_db}
+hmmsearch --noali --cpu 20 -E 0.001 --tblout ${HMMsearch}/PF00999Bachmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv ${HMMdir} ${Bac_db}
 
 module load Python/3.11.6-foss-2023a
-python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Bachmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Arc_TSV} ${HMMsearch}/PF00999Bachmmvs_vsArchaea_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
-
+python getting_fasta_from_hit_extra_Arc.py ${HMMsearch}/PF00999Bachmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03.tsv HMM ${Bac_TSV} ${HMMsearch}/PF00999Bachmmvs_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_fl_sequence.fasta
 
 #---------------------------------------------------------NhaC------------------------------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
