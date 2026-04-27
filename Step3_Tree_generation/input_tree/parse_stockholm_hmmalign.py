5750e2aa6b2670fd223a386e326211ecd8f8f3a9
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-04-28 11:48:15 +1200

    Update parse_stockholm_hmmalign.py

--- Step3_Tree_generation/input_tree/parse_stockholm_hmmalign.py
+++ Step3_Tree_generation/input_tree/parse_stockholm_hmmalign.py
@@ -4,7 +4,7 @@ import sys
 hit_file = sys.argv[1] # '/nesi/nobackup/uc04105/results/hmmalign/pipeline/CPA/Archaea_nhaA_TRKACpfam_align_CPAfold.sthk'  #sys.argv[1]  
 outfile = sys.argv[2]
 # define alignment
-
+#this misses a size exclusion, but since they all did the 70% alinment for HMMscan they are fine
 alignment={}
 
 
