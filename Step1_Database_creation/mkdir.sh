037eb35aff48d67e5409a16f4a7f9561d5e903c3
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-05-29 13:53:28 +1200

    Update mkdir.sh

--- Step1_Database_creation/mkdir.sh
+++ Step1_Database_creation/mkdir.sh
@@ -2,9 +2,22 @@ mkdir ~/GTDB_226
 mkdir ~/GTDB_226/Bac_DB
 mkdir ~/GTDB_226/Bac_DB/tsv
 mkdir ~/GTDB_226/Bac_DB/fasta
-mkdir ~/GTDB_226/Arc_DB
-mkdir ~/GTDB_226/Arc_DB/tsv
-mkdir ~/GTDB_226/Arc_DB/fasta
+mkdir ~/GTDB_226/ArcDB
 mkdir ~/HMM
+mkdir ~/HMM/PF00999
 mkdir ~/results/MMseqs/
+mkdir ~/results/MMseqs/Eukarya
+mkdir ~/results/MMseqs/Bacteria
+mkdir ~/results/MMseqs/Archaea
+mkdir ~/results/MMseqs/Eukarya/part2
+mkdir ~/results/MMseqs/Bacteria/part2
+mkdir ~/results/MMseqs/Archaea/part2
 mkdir ~/results/HMMsearch/
+mkdir ~/results/HMMsearch/PF00999
+mkdir ~/results/HMMsearch/Archaea
+mkdir ~/results/HMMsearch/Bacteria
+mkdir ~/results/HMMsearch/Eukarya
+
+mkdir ~/results/HMMalign/
+mkdir ~/results/HMMalign/PF00999
+
