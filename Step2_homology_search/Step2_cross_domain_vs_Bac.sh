d00f1a633d32a4fe8902c0266b394dfcb3363b0d
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-05-29 13:51:40 +1200

    Update Step2_cross_domain_vs_Bac.sh

--- Step2_homology_search/Step2_cross_domain_vs_Bac.sh
+++ Step2_homology_search/Step2_cross_domain_vs_Bac.sh
@@ -10,23 +10,22 @@
 
 declare -a array=($(seq 0 127))
 
-HMMdir=/nesi/nobackup/uc04105/cross_biome_metagenome/Protein/HMM/PF00999.hmm
-Bac_TSV=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bac_DB/tsv/Bacteria_GTDB226_protein_May92025_chunk_${array[$SLURM_ARRAY_TASK_ID]}.tsv
-Bac_db=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bac_DB/fasta/Bacteria_GTDB226_protein_May92025_subset${array[$SLURM_ARRAY_TASK_ID]}.fasta
-HMMsearch=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMsearch/Bacteria
-MMseqs=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/MMseq/Bacteria/PF00999/part2
-Seq=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/Bacteria_all_part1A_remove_dupes.fasta
-
+HMMdir=~/HMM/PF00999.hmm
+Bac_TSV=~/tsv/Bacteria_GTDB226_protein_May92025_chunk_${array[$SLURM_ARRAY_TASK_ID]}.tsv
+Bac_db=~/Bac_DB/fasta/Bacteria_GTDB226_protein_May92025_subset${array[$SLURM_ARRAY_TASK_ID]}.fasta
+HMMsearch~/results/HMMsearch/Bacteria
+MMseqs=~/results/MMseq/Bacteria/PF00999/part2
+Seq=~/results/Bacteria_all_part1A_remove_dupes.fasta
 module load MMseqs2/15-6f452-gompi-2023a
 
 mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 20 ${Seq}  ${Bac_db} ${MMseqs}/PF00999Bacseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
 
 rm -r ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
-Seq=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/Archaea_all_part1A_remove_dupes.fasta
+Seq=~/results/Archaea_all_part1A_remove_dupes.fasta
 mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 20 ${Seq}  ${Bac_db} ${MMseqs}/PF00999Arcseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
 
 rm -r ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
-Seq=/home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/Euk_database_May/results/Eukarya_part1A_dupes_removed_sequences.fasta
+Seq=~/results/Eukarya_part1A_dupes_removed_sequences.fasta
 mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 20 ${Seq}  ${Bac_db} ${MMseqs}/PF00999Eukseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
 
 rm -r ${array[$SLURM_ARRAY_TASK_ID]}_Bac_tmp
