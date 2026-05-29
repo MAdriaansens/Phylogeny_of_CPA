7bf315ff1988a74fcf4be7fde61b8d1fe9468a8c
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-05-29 15:13:46 +1200

    Update hmmalign_filter_part2output_for_part3MMseq_bacteria.sh

--- Step2_homology_search/hmmalign_filter_part2output_for_part3MMseq_bacteria.sh
+++ Step2_homology_search/hmmalign_filter_part2output_for_part3MMseq_bacteria.sh
@@ -9,9 +9,9 @@
 #SBATCH --array         0-123
 
 declare -a array=($(seq 4 127))
-HMMalign=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/results/HMMalign/Bacteria/PF00999/part2
-HMM=/nesi/nobackup/uc04105/cross_biome_metagenome/Protein/HMM/PF00999.hmm
-Seq=/home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/GTDB_226/results/MMseq/Bacteria/PF00999/part2/PF00999Arcseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
+HMMalign=~/results/HMMalign/Bacteria/PF00999/part2
+HMM=~/HMM/PF00999.hmm
+Seq=~/results/MMseq/Bacteria/PF00999/part2/PF00999Arcseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
 module load HMMER/3.4-GCC-12.3.0
 module load Python/3.11.6-foss-2023a
 
@@ -19,13 +19,13 @@ hmmalign --amino --trim -o ${HMMalign}/ArcPF00999_vsBacteria_subset${array[$SLUR
 
 python parse_stockholm_filterFL.py ${HMMalign}/ArcPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.sthk ${HMMalign}/ArcPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_FL_e03_mmseq_alignedPF00999.fasta 263
 
-Seq=/home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/GTDB_226/results/MMseq/Bacteria/PF00999/part2/PF00999Eukseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
+Seq=~/results/MMseq/Bacteria/PF00999/part2/PF00999Eukseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
 hmmalign --amino --trim -o ${HMMalign}/EukPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.sthk ${HMM} ${Seq}
 
 python parse_stockholm_filterFL.py ${HMMalign}/EukPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.sthk ${HMMalign}/EukPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_FL_e03_mmseq_alignedPF00999.fasta 263
 
 
-Seq=/home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/GTDB_226/results/MMseq/Bacteria/PF00999/part2/PF00999Bacseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
+Seq=~/results/MMseq/Bacteria/PF00999/part2/PF00999Bacseq_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq_fl_seq.fasta
 
 hmmalign --amino --trim -o ${HMMalign}/BacPF00999_vsBacteria_subset${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.sthk ${HMM} ${Seq}
 
