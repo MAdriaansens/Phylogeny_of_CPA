666cd7c37be45836bc6744d4990d9835ffcb7f09
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-05-29 15:17:18 +1200

    Update Step1_Cluster_diff_seqid.sh

--- Step3_Tree_generation/Step1_Cluster_diff_seqid.sh
+++ Step3_Tree_generation/Step1_Cluster_diff_seqid.sh
@@ -10,8 +10,8 @@
 module load MMseqs2/15-6f452-gompi-2023a
 declare -a array=(0.6 0.65 0.7 0.75 0.8 0.85 0.9 1)
 
-mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Archaea_April_hmmaligned_e03_mmseq.fasta.fa /home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/final_april28/tree_input/Archaea_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
+mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Archaea_April_hmmaligned_e03_mmseq.fasta.fa /`/tree_input/Archaea_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
 
-mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Eukarya_April_hmmaligned_e03_mmseq.fasta.fa /home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/final_april28/tree_input/Eukarya_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
+mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Eukarya_April_hmmaligned_e03_mmseq.fasta.fa ~/tree_input/Eukarya_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
 
-mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Bacteria_April_hmmaligned_e03_mmseq.fasta.fa /home/mad149/00_nesi_projects/uc04105_nobackup/new_databases_May/final_april28/tree_input/Bacteria_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
+mmseqs easy-cluster --threads 12 -c 0.0 --min-seq-id 0.7 ~/tree_input/Bacteria_April_hmmaligned_e03_mmseq.fasta.fa ~/tree_input/Bacteria_April_hmmaligned_e03_mmseq_treeinput_clusterd_at_${array[$SLURM_ARRAY_TASK_ID]}.faa  tmp_${array[$SLURM_ARRAY_TASK_ID]}
