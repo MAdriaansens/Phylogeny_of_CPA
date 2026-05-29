6e4ac4ad2324dcc6c711764d95752b14ddb5c93d
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-05-29 12:40:32 +1200

    Create mkdir.sh

---
+++ Step1_Database_creation/mkdir.sh
@@ -0,0 +1,7 @@
+mkdir ~/GTDB_226
+mkdir ~/GTDB_226/Bac_DB
+mkdir ~/GTDB_226/Bac_DB/tsv
+mkdir ~/GTDB_226/Bac_DB/fasta
+mkdir ~/GTDB_226/Arc_DB
+mkdir ~/GTDB_226/Arc_DB/tsv
+mkdir ~/GTDB_226/Arc_DB/fasta
